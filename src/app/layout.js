4aad0030a5b94f36b2f8ada9e3e794be6cdb61f8
feehrodrigues <feehrodriguesoficial@gmail.com>
2026-01-10 12:12:54 -0300

    Corrigindo fonte Inter e layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,12 @@ import { ClerkProvider } from '@clerk/nextjs'
 import { Inter } from 'next/font/google' // Importa a fonte do Google
 import './globals.css'
 
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
+
+// Adicione a meta tag de verificação do Google aqui
 <meta name="google-site-verification" content="OEuc9eBot0a7AMQ8ZNUiDWrylytBHU4_ey3tUV4Qr2s" />
+
+
 
 
 export const metadata = {
